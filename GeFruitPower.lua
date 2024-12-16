7a201d90533bb0bddb40ca189ac7ebfa5a389710
Laelmano24 <brockbomba@gmail.com>
2024-12-16 07:05:43 -0300

    update GeFruitPower.lua

---
+++ GeFruitPower.lua
@@ -0,0 +1,20 @@
+return {
+  "Invisible Power",
+  "Fly Power",
+  "Bomb Power",
+  "Diamond Power",
+  "Spin Power",
+  "Barrier Power",
+  "Moai Power",
+  "Sand Power",
+  "Ice Power",
+  "Water Power",
+  "Gold Power",
+  "Snow Power",
+  "Paw Power",
+  "Flame Power",
+  "Dough Power",
+  "Dog Power",
+  "Dark Power",
+  "Floppa Power"
+}
